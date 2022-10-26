7e0bd349fb918e07fa62bb85c09496f77d608a25
surakshit-baccha <93087724+surakshit-baccha@users.noreply.github.com>
2022-10-27 01:29:44 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,21 +1,37 @@
 import React from 'react'
-import './Alert.css'
+import styles from "./Alert.css";
+
 const Alert = ({ handleOK, handleCancel, alertVis }) => {
     return (
-        <div className="modal" style={{ display: alertVis ? "block" : "none" }}>
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-body">
-                        <p>Are you Sure?</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleCancel}>No</button>
-                        <button type="button" className="btn btn-primary" onClick={handleOK}>Delete</button>
+        <>
+            <div className="modal" style={{ display: alertVis ? "block" : "none" }}>
+                <div className={styles.darkBG} onClick={handleCancel} />
+                <div className={styles.centered}>
+                    <div className={styles.modal}>
+                        <div className={styles.modalHeader}>
+                            <h5 className={styles.heading}>Dialog</h5>
+                        </div>
+                        <div className={styles.modalContent}>
+                            Are you sure you want to delete this?
+                        </div>
+                        <div className={styles.modalActions}>
+                            <div className={styles.actionsContainer}>
+                                <button className={styles.deleteBtn} onClick={handleOK}>
+                                    Delete
+                                </button>
+                                <button
+                                    className={styles.cancelBtn}
+                                    onClick={handleCancel}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
-export default Alert
+export default Alert;
